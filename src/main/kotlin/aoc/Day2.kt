88834e7bf662b77708d31b8a61ef9f0f629b2f79
Rahul Popuri <rpopuri@tripactions.com>
2021-12-02 07:25:47 -0800

    cleanup

--- src/main/kotlin/aoc/Day2.kt
+++ src/main/kotlin/aoc/Day2.kt
@@ -1,17 +1,15 @@
 package aoc
 
-import aoc.toStringList
-
 fun main() {
     val input = loadInput()
     println("Part 1: " + part1(input))
     println("Part 2: " + part2(input))
 }
 
-fun part1(input: Array<Instruction>): Int {
+fun part1(input: List<Instruction>): Int {
     var x = 0
     var y = 0
-    for (instruction in input) {
+    input.forEach { instruction ->
         when (instruction.command) {
             "forward" -> x += instruction.value
             "down" -> y += instruction.value
@@ -21,11 +19,11 @@ fun part1(input: Array<Instruction>): Int {
     return x * y
 }
 
-fun part2(input: Array<Instruction>): Int {
+fun part2(input: List<Instruction>): Int {
     var x = 0
     var y = 0
     var aim = 0
-    for (instruction in input) {
+    input.forEach { instruction ->
         when (instruction.command) {
             "forward" -> {
                 x += instruction.value
@@ -40,8 +38,8 @@ fun part2(input: Array<Instruction>): Int {
 
 data class Instruction(val command: String, val value: Int)
 
-fun loadInput(): Array<Instruction> {
+fun loadInput(): List<Instruction> {
     return "input_day2.txt".toStringList()
-        .map { Instruction(it.split(" ")[0], it.split(" ")[1].toInt()) }
-        .toTypedArray()
+        .map { val split = it.split(" "); Instruction(split[0], split[1].toInt()) }
+        .toList()
 }
